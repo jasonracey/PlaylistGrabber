3dc7e948912507d3f894c11cbd4ec7c4be3d69fe
Jason Racey <jasonracey@gmail.com>
2020-12-05 07:32:39 -0800

    Add unit test project

---
+++ PlaylistGrabber.Test/DownloaderTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlaylistGrabber.Test
+{
+    [TestClass]
+    public class DownloaderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
